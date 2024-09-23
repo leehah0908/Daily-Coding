db1213f15356494a6cb2db709f041c89e51bf597
이한희 <87411703+leehah0908@users.noreply.github.com>
2024-09-23 17:31:19 +0900

    [Silver IV] Title: 카드2, Time: 224 ms, Memory: 48908 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
